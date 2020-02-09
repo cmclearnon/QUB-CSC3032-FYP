da07c8ac44eb418267713756d7f2c63bb0f8dd92
Chris McLearnon <chrismclearnon@icloud.com>
2020-02-09 20:49:53 +0000

    Feature extraction & dataset custom transformers

--- src/ml/featureprocessing/FeatureEngineering.py
+++ src/ml/featureprocessing/FeatureEngineering.py
@@ -1,83 +1,101 @@
 import urllib.parse
+import whois
 import re
 from tld import get_tld, is_tld
 
-def extract(url):
-    #url_dt = []
-    url_dt = {}
-    """
-    This function extracts all Lexical based features of the URL string
-    """
+def clean_data(url):
+    url_obj = urlparse(url)
+    if not url_obj.scheme:
+        url = "http://" + url
+        return url
+    return url
 
-    # TODO: Length of URL
+def lexical_extract(url):
+    """
+    Extract lexical based features from the URL strings in the dataset. These features include:
+    - Length of URL
+    - Length of Hostname
+    - Path length
+    - TLD Length
+    - Special Char Count
+    - '.-@%=?' Char Count
+    - Digit Count
+    - Unique Char Count"""
+
+    if not url:
+        return
+
+    url_dt = {}
+
     url_len = len(url)
-    #url_dt.append(url_len)
     url_dt.update({"URLLength": url_len})
 
-    # TODO: Length of Hostname
     url_rg = '(?:http.*://)?(?P<host>[^:/ ]+).*'
     url_search = re.search(url_rg, url)
     host_len = len(url_search.group('host'))
-    #url_dt.append(host_len)
     url_dt.update({"HostLength": host_len})
-    #host_len = len(urlparse(url).hostname)
 
     # TODO: Length of Path
 
-    # TODO: Length of Top Level Domain
     parsed_tld = get_tld(url, fix_protocol=True, fail_silently=True)
     if parsed_tld is None:
         url_dt.update({"TLDLength": 0})
     else:
         tld_len = len(parsed_tld)
         url_dt.update({"TLDLength": tld_len})
-    # elif not is_tld(url):
-    #     url_dt.update({"TLDLength": 0})
 
     # TODO: Count of special characters
 
-    # TODO: Count of '.'
     dot_count = str(url).count('.')
-    #url_dt.append(dot_count)
     url_dt.update({"DotCount": dot_count})
 
-    # TODO: Count of '-'
     dash_count = str(url).count('-')
-    #url_dt.append(dash_count)
     url_dt.update({"DashCount": dash_count})
 
-    # TODO: Count of '@'
     at_count = str(url).count('@')
-    #url_dt.append(at_count)
     url_dt.update({"@Count": at_count})
 
-    # TODO: Count of '%'
     percent_count = str(url).count('%')
-    #url_dt.append(percent_count)
     url_dt.update({"%Count": percent_count})
 
-    # TODO: Count of '='
     equals_count = str(url).count('=')
-    #url_dt.append(equals_count)
     url_dt.update({"=Count": equals_count})
 
-    # TODO: Count of '?'
     question_mark_count = str(url).count('?')
-    #url_dt.append(question_mark_count)
     url_dt.update({"?Count": question_mark_count})
 
     # TODO: Is executable
 
-    # TODO: Count of digits
+
     d_count = sum(list(map(lambda x:1 if x.isdigit() else 0, list(url))))
-    #url_dt.append(d_count)
     url_dt.update({"DigitCount": d_count})
 
-    # TODO: Count of unique characters
     un_ch_count = len(set(url))
-    #url_dt.append(un_ch_count)
     url_dt.update({"UniqueCharCount": un_ch_count})
 
     # TODO: Is IP address
+
+
     return url_dt
+
+
+def host_extract(url):
+    """
+    This function extracts any domain specific features from the URL to add to the dataset model
+    """
+
+    # Registry date of Domain
+    url_regex = '(?:http.*://)?(?P<host>[^:/ ]+).*'
+    url_search = re.search(url_regex, url)
+    hostname = url_search.group('host')
+
+    domain = whois.query(hostname)
+    reg_date = domain.creation_date
+    print(reg_date)
+
+    # TODO: PageRank
+
+    # TODO: Google Index
+
+    # TODO: Is in Alexa Top 1 Million Websites
 

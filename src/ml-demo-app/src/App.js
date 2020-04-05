cc10dc454d65d568c6566046c34812bc19283e75
Chris McLearnon <chrismclearnon@icloud.com>
2020-04-05 22:39:33 +0100

    Package.json file

--- src/ml-demo-app/src/App.js
+++ src/ml-demo-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import URLForm from './components/URLForm';
 import DatasetTable from './components/DatasetTable';
+// import ModelAccuracy from './components/views/Dashboard/components/ModelAccuracy';
 import Prediction from './components/Prediction';
 import { Box, Heading, Header, Anchor, Footer, Title, Menu, Button, Grommet, ResponsiveContext, Grid, Sidebar } from 'grommet';
 import { Text } from 'grommet';
@@ -114,57 +115,21 @@ class App extends React.Component {
             ))}
           </Box>
         <Box 
+          direction="row"
           gridArea="main"
           justify="center"
           align="center"
           pad={{ horizontal: "medium", vertical: "small" }}
         >
-          {/* <DatasetTable items={this.state.dataset} total={this.state.total}/> */}
-          <DatasetTable/>
+          <Box 
+            pad={{ horizontal: "medium", vertical: "small" }}
+          >
+            {/* <ModelAccuracy/> */}
+            <DatasetTable/>
+          </Box>
         </Box>
       </Grid>
     </Grommet>
-      // <Grommet theme={theme} full>
-      //         <ResponsiveContext.Consumer>
-      //     {size => (
-      //       <Box fill>
-      //         <AppBar>
-      //           <Heading level='3' margin='none'>Malicious URL Detection w/ML</Heading>
-      //         </AppBar>
-      //         <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-      //           <Box flex align='left' justify='centre' background='light-5'>
-      //             <Grid
-      //               rows={['xxsmall', 'medium']}
-      //               columns={['medium', 'medium']}
-      //               gap="small"
-      //               areas={[
-      //                 { name: 'header', start: [0, 0], end: [1, 0] },
-      //                 { name: 'nav', start: [0, 1], end: [0, 1] },
-      //                 { name: 'main', start: [1, 1], end: [1, 1] },
-      //               ]}
-      //             >
-      //               <Box gridArea="header" background="teal" />
-      //               <Box gridArea="nav" background="white">
-      //                 <form onSubmit={this.handleSubmit}>
-      //                     {/* <input name="url" type="text" placeholder="URL" value={this.state.url} onChange={this.handleURLChange}/> */}
-      //                     <TextInput
-      //                       placeholder="URL"
-      //                       value={this.state.url}
-      //                       onChange={this.handleURLChange}
-      //                     />
-      //                     {/* <button>Predict</button> */}
-      //                     <Button label='Submit' onClick={this.handleSubmit} />
-      //                     <Prediction predProba = {this.state.pred_proba}/>
-      //                 </form>
-      //               </Box>
-      //               <Box gridArea="main" background="white" />
-      //             </Grid>
-      //           </Box>
-      //         </Box>
-      //       </Box>
-      //     )}
-      //   </ResponsiveContext.Consumer>
-      // </Grommet>
     );
   }
 }

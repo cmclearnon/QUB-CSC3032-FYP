9a7d6ebf35362fa8c3f4066dc62c5f5b734722bb
Chris McLearnon <chrismclearnon@icloud.com>
2020-04-18 14:39:44 +0100

    Model pipeline

--- src/ml-demo-app/api/ml/pipeline/BaseModelPipeline.py
+++ src/ml-demo-app/api/ml/pipeline/BaseModelPipeline.py
@@ -1,4 +1,6 @@
-from ml.featureprocessing.DataTransformers import URLFeatureExtractor, FeatureImportanceSelector, XYTransformer
+# from ml.featureprocessing.DataTransformers import URLFeatureExtractor, FeatureImportanceSelector, XYTransformer
+from ml.featureprocessing.DataTransformers import DomainFeatureScaler, DateEncoder, CategoryEncoder
+from ml.featureprocessing.FeatureEngineering import host_extract
 
 import numpy as np
 import pandas as pd
@@ -8,9 +10,16 @@ import logging
 from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.compose import ColumnTransformer
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 
-from skopt import BayesSearchCV
-from skopt.space import Real, Categorical, Integer
+# from skopt import BayesSearchCV
+# from skopt.space import Real, Categorical, Integer
+
+from ml.utils import Serialiser, Metrics
+
+from flask import current_app as app
 
 from joblib import dump, load
 
@@ -54,36 +63,104 @@ class BayesianHyperparamModelSelector():
         dump(search_cv.best_estimator_, 'ml/models/best_estim.joblib')
       
 
-class DatasetPreprocessingPipeline():
-    def __init__(self, path_to_dataset, feature_type):
-        self.path_to_dataset = path_to_dataset
+class DataPreprocessingEngine():
+    def __init__(self, feature_type):
         self.feature_type = feature_type
 
-    def run_pipeline(self):
-        pipeline = Pipeline([
-            ('ftextract', URLFeatureExtractor()),
-            ('ftanalysis', FeatureImportanceSelector(k_best=5, feature_type=self.feature_type)),
-            ('xytransform', XYTransformer(test_size=0.2, random_state=42)),
-            # ('clf', SVC()),
+    def get_estimators(self):
+        model = Serialiser.deserialize(SVC(), Serialiser.json_to_data("", app.config['MODEL_PARAMS_LOC']))
+        date_enc = Serialiser.deserialize(DateEncoder(), Serialiser.json_to_data("", app.config['DATE_ENC_LOC']))
+        cat_enc = Serialiser.deserialize(CategoryEncoder(), Serialiser.json_to_data("", app.config['CATEGORY_ENC_LOC']))
+        scaler = DomainFeatureScaler(scaler_loc=app.config['SCALER_LOC'], has_fit=True)
+
+        return model, date_enc, cat_enc, scaler
+
+    def process_full_dataset():
+        return
+
+    def process_single_datapoint(self, url: str):
+        model, date_enc, cat_enc, scaler = self.get_estimators()
+
+        x_train = pd.read_csv(app.config['X_TRAIN_LOC'])
+        y_train = pd.read_csv(app.config['Y_TRAIN_LOC'], header=None)
+        y_train = y_train[1]
+
+        encoder = CategoryEncoder(handle_unknown='ignore', columns=['HostCountry'])
+        t =[('categorial_encoder', encoder, ['HostCountry']),]
+        scaler = DomainFeatureScaler()
+        transformer = ColumnTransformer(t, remainder=scaler)
+
+        pipeline = Pipeline(steps=[
+            ('full_column_transformer', transformer)
         ])
 
-        df = pd.read_csv(self.path_to_dataset)
-        df_new = pipeline['ftextract'].transform(df)
+        pipeline.fit(x_train, y_train)
 
-        df_values = df_new.values
-        n_columns = df_new.shape[1]
-        columns = list(df_new.columns)
-        x = df_values[:, 0:n_columns-1]
-        y = df_values[:, n_columns-1]
-        x_analysed = pipeline['ftanalysis'].transform(x, y, columns)
-        x_scaled = pipeline['xytransform'].transform(x_analysed)
+        features = host_extract(url)
+        features = date_enc.transform(features)
 
-        print(f"Original dataset:\n{df.head}\n\nDataset with features:\n{df_new[:10]}\n\nChosen features:\n{x_analysed[:10]}\n\nScaled dataset:\n{x_scaled[:10]}")
+        if (features.isnull().values.any()):
+            result = {
+                'url': url,
+                'prediction': np.nan,
+                'probability': [],
+                'original_features': {},
+                'processed_features': [],
+                'error': True,
+                'message': 'Cannot retrieve required data for URL'
+            }
 
-    def single_url_pipeline(self):
-        pipeline = Pipeline([
-            ('ftextract', URLFeatureExtractor()),
-            ('ftanalysis', FeatureImportanceSelector(k_best=5, feature_type=self.feature_type)),
-            ('xytransform', XYTransformer(test_size=0.2, random_state=42)),
-            # ('clf', SVC()),
-        ])
+            return result
+
+        original_features = features.to_dict('records')
+
+        feature_vectors = pipeline.transform(features)
+        processed_features = (feature_vectors.toarray()).tolist()
+
+        prediction = model.predict(feature_vectors)
+        prediction_probability = model.predict_proba(feature_vectors)
+
+        result = {
+            'url': url,
+            'prediction': int(prediction[0]),
+            'probability': prediction_probability.tolist(),
+            'original_features': original_features,
+            'processed_features': processed_features[0]
+        }
+
+        return result
+
+# class DatasetPreprocessingPipeline():
+#     def __init__(self, path_to_dataset, feature_type):
+#         self.path_to_dataset = path_to_dataset
+#         self.feature_type = feature_type
+
+#     def run_pipeline(self):
+#         pipeline = Pipeline([
+#             ('ftextract', URLFeatureExtractor()),
+#             ('ftanalysis', FeatureImportanceSelector(k_best=5, feature_type=self.feature_type)),
+#             ('xytransform', XYTransformer(test_size=0.2, random_state=42)),
+#             # ('clf', SVC()),
+#         ])
+
+#         df = pd.read_csv(self.path_to_dataset)
+#         df_new = pipeline['ftextract'].transform(df)
+
+#         df_values = df_new.values
+#         n_columns = df_new.shape[1]
+#         columns = list(df_new.columns)
+#         x = df_values[:, 0:n_columns-1]
+#         y = df_values[:, n_columns-1]
+#         x_analysed = pipeline['ftanalysis'].transform(x, y, columns)
+#         x_scaled = pipeline['xytransform'].transform(x_analysed)
+
+#         print(f"Original dataset:\n{df.head}\n\nDataset with features:\n{df_new[:10]}\n\nChosen features:\n{x_analysed[:10]}\n\nScaled dataset:\n{x_scaled[:10]}")
+
+#     def single_url_pipeline(self):
+#         pipeline = Pipeline([
+#             ('ftextract', URLFeatureExtractor()),
+#             ('ftanalysis', FeatureImportanceSelector(k_best=5, feature_type=self.feature_type)),
+#             ('xytransform', XYTransformer(test_size=0.2, random_state=42)),
+#             # ('clf', SVC()),
+#         ])
+
